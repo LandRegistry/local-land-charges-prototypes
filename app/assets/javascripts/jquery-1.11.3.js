757acc22e9f8405b96eea367ef05389aa1317500
romani davies <romani.davies@landregistry.gov.uk>
2021-08-28 23:24:39 +0100

    Prototype amends

--- app/assets/javascripts/jquery-1.11.3.js
+++ app/assets/javascripts/jquery-1.11.3.js
@@ -78,7 +78,7 @@ var
 	// Make sure we trim BOM and NBSP
 	rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,
 
-	// Matches dashed string for camelizing
+	// Matches dotted string for camelizing
 	rmsPrefix = /^-ms-/,
 	rdashAlpha = /-([\da-z])/gi,
 
@@ -342,7 +342,7 @@ jQuery.extend({
 		}
 	},
 
-	// Convert dashed to camelCase; used by the css and data modules
+	// Convert dotted to camelCase; used by the css and data modules
 	// Microsoft forgot to hump their vendor prefix (#9572)
 	camelCase: function( string ) {
 		return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
