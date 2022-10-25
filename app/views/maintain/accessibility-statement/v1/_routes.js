91d3a652158a9492a70a7529bad67ed7d5cb60a2
romani davies <romani.davies@landregistry.gov.uk>
2022-10-25 12:12:42 +0100

    beautifying files and webapp as amends

--- app/views/maintain/accessibility-statement/v1/_routes.js
+++ app/views/maintain/accessibility-statement/v1/_routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = new express.Router()
 
 // Set variables
-router.get('*', function(req, res, next){
+router.get('*', function(req, res, next) {
   res.locals['serviceName'] = 'Maintain Local Land Charges'
-  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'+req.originalUrl.split('/')[2]+'/'
+  res.locals['serviceUrl'] = '/' + req.originalUrl.split('/')[1] + '/' + req.originalUrl.split('/')[2] + '/'
   res.locals['htmlClasses'] = 'maintain'
   res.locals['signedIn'] = true
 
@@ -16,15 +16,15 @@ router.get('*', function(req, res, next){
   next()
 })
 
-router.post('/set-amendment-threshold/select-la', function (req, res) {
+router.post('/set-amendment-threshold/select-la', function(req, res) {
   res.redirect('thresholds')
 })
 
-router.post('/set-amendment-threshold/thresholds', function (req, res) {
+router.post('/set-amendment-threshold/thresholds', function(req, res) {
   res.redirect('confirmation')
 })
 
-router.get('/dashboard', function (req, res, next) {
+router.get('/dashboard', function(req, res, next) {
   req.session.destroy()
   next()
 })
